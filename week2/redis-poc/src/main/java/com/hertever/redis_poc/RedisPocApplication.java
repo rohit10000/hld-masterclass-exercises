06690de6c43c4aa1e92a1c96dd3527b5945a6355
rohitsingh <rohitsingh18997@gmail.com>
2025-08-24 06:59:54 +0530

    initialize redis-poc

---
+++ week2/redis-poc/src/main/java/com/hertever/redis_poc/RedisPocApplication.java
@@ -0,0 +1,39 @@
+package com.hertever.redis_poc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@SpringBootApplication
+public class RedisPocApplication implements CommandLineRunner {
+
+	private static final Log LOG = LogFactory.getLog(RedisPocApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisPocApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory();
+		connectionFactory.afterPropertiesSet();
+
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.setDefaultSerializer(StringRedisSerializer.UTF_8);
+		template.afterPropertiesSet();
+
+		template.opsForValue().set("foo", "bar");
+
+		LOG.info("Value at foo:" + template.opsForValue().get("foo"));
+
+		connectionFactory.destroy();
+	}
+}
+
+
---
+++ week2/redis-poc/src/test/java/com/hertever/redis_poc/RedisPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hertever.redis_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
